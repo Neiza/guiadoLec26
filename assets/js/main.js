2167c70f43afb9ea1d331ca9bae2e6a60ca02523
Neiza Nunez <neizapf@gmail.com>
2017-04-27 15:44:52 -0500

    primer

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+var inputName = document.getElementsByTagName("input")[0];
+var inputLastName = document.getElementsByTagName("input")[1];
+var inputDni = document.getElementsByTagName("input")[2];
+var inputDate = document.getElementsByTagName("input")[3];
+var inputCellphone = document.getElementsByTagName("input")[4];
+var inputphone = document.getElementsByTagName("input")[5];
+var inputPassword = document.getElementsByTagName("input")[6];
+var inputPassword2 = document.getElementsByTagName("input")[7];
+var post = document.getElementsByTagName("textarea")[0];
+var inputCheckbox = document.getElementsByTagName("input")[8];
+var button = document.getElementsByTagName("input")[9];
+
+
+var expressionOnlyLetters          = /^[A-Za-z]+$/;
+var expressionFirstLetterUppercase = /^[A-Z]/;
+var expressionEmail                = /\w+@+\w+\.+[a-z]/;
+var expressionPassword6Characters  =/^[A-Za-z0-9_]$/;
+
+ function nameLastnameValidation(){
+  if  (!expressionOnlyLetters.test(inputName.value)){
+     alert("no");
+   }
+ }
+
+ nameLastnameValidation();
